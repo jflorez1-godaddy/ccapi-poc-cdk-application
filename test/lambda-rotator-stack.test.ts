da295a8781d54cd322c6e6885cea03b7dd69b579
jflorez1 <jflorez1@godaddy.com>
2025-02-12 11:29:39 -0500

    Initial commit

---
+++ test/lambda-rotator-stack.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LambdaRotatorStack from '../lib/lambda-rotator-stack-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lambda-rotator-stack-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LambdaRotatorStack.LambdaRotatorStackStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
